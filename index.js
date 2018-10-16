ad7beccd0d3aab1595dce69674b4ee73269b1205
Lauren Piontek <piontekle@gmail.com>
2018-10-16 02:23:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+ const lis = document.querySelectorAll('ul.ranked-list li');
+ for (var i=0; i < lis.length; i++) {
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+ }
+}
+
+function deepestChild() {
+  var elements = document.querySelector('#grand-node div');
+  var nextElement = elements.children[0];
+  while (nextElement) {
+    elements = nextElement;
+    nextElement = elements.children[0];
+  }
+  return elements;
+}
